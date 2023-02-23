af8a2c36b6b07b5f8dbc52d6790893189ebe640c
sammyshakes <sam.shakespeare@gmail.com>
2023-02-22 23:15:45 -0500

    deploy contract builder

--- cli/src/cmd/forge/zksync_deploy.rs
+++ cli/src/cmd/forge/zksync_deploy.rs
@@ -11,25 +11,38 @@ use std::io::{Read, Result};
 use zksync::operations::DeployContractBuilder;
 use zksync::types::H256;
 use zksync::zksync_eth_signer::{EthereumSigner, PrivateKeySigner};
+use zksync::zksync_types::L2ChainId;
+use zksync::{ethereum, EthereumProvider};
 use zksync::{signer, wallet};
 
-pub fn deploy_zksync(config: &Config, project: &Project) -> Result<()> {
+pub async fn deploy_zksync(config: &Config, project: &Project) -> Result<()> {
     // let rpc_url = config.get_rpc_url().unwrap().ok();
     // println!("{:#?}, rpc_url ---->>>", rpc_url);
     // println!("{:#?}, config ---->>>", config.rpc_endpoints.endpoints["goerli"]);
 
-    // get signer
-    // let _signer = signer::Signer::new();
+    // connect to the network
+    // let client = Provider::<Http>::try_from("https://zksync2-testnet.zksync.dev").unwrap();
+    let client =
+        Provider::<Http>::try_from("https://goerli.infura.io/v3/332aa765e52d4f219b8408485be193c1")
+            .unwrap();
+
     let private_key = H256::from([5; 32]);
     let eth_signer = PrivateKeySigner::new(private_key);
     // println!("{:#?}, eth_signer ---->>>", eth_signer.get_address());
-    let signer_addy = eth_signer.get_address();
+    let signer_addy = eth_signer.get_address().await.unwrap();
+    // get signer
+    let _signer = signer::Signer::new(eth_signer, signer_addy, L2ChainId(280));
 
     // // create a wallet
-    // let wallet = Wallet::new();
+    let wallet = wallet::Wallet::with_http_client(
+        "https://goerli.infura.io/v3/332aa765e52d4f219b8408485be193c1",
+        _signer,
+    )
+    .unwrap();
 
     // Build Deployer
-    // let deployer_builder = DeployContractBuilder::new(wallet);
+    let deployer_builder = DeployContractBuilder::new(&wallet);
+    println!("{:#?}, deployer_builder ---->>>", deployer_builder);
 
     //get abi and bytecode
     let output_path: &str = &format!("{}{}", project.paths.root.display(), "/zksolc/combined.json");
@@ -82,11 +95,6 @@ pub fn deploy_zksync(config: &Config, project: &Project) -> Result<()> {
     let bytecode_hash_bytes = bytecode_hash.bytes();
     // println!("{:#?}, bytecode_hash_bytes ---->>>", bytecode_hash_bytes);
 
-    // connect to the network
-    // let client = Provider::<Http>::try_from("https://zksync2-testnet.zksync.dev").unwrap();
-    let client =
-        Provider::<Http>::try_from("https://goerli.infura.io/v3/332aa765e52d4f219b8408485be193c1")
-            .unwrap();
     let client = std::sync::Arc::new(client);
     println!("{:#?}, client url---->>>", client.url());
     // println!("{:#?}, project ---->>>", project);
