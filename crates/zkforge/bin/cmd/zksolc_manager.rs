f08160bc15366d8e0616322d4594708646f075ae
Dustin Brickwood <dustinbrickwood204@gmail.com>
2023-10-30 12:45:48 -0500

    chore: adds latest zksolc compiler (#135)

--- crates/zkforge/bin/cmd/zksolc_manager.rs
+++ crates/zkforge/bin/cmd/zksolc_manager.rs
@@ -59,10 +59,10 @@ pub enum ZkSolcVersion {
     V1311,
     V1313,
     V1314,
-    V1315,
+    V1316,
 }
 
-pub const DEFAULT_ZKSOLC_VERSION: &str = "v1.3.15";
+pub const DEFAULT_ZKSOLC_VERSION: &str = "v1.3.16";
 
 /// `parse_version` parses a string representation of a `zksolc` compiler version
 /// and returns the `ZkSolcVersion` enum variant if it matches a supported version.
@@ -86,7 +86,7 @@ fn parse_version(version: &str) -> Result<ZkSolcVersion> {
         "v1.3.11" => Ok(ZkSolcVersion::V1311),
         "v1.3.13" => Ok(ZkSolcVersion::V1313),
         "v1.3.14" => Ok(ZkSolcVersion::V1314),
-        "v1.3.15" => Ok(ZkSolcVersion::V1315),
+        "v1.3.16" => Ok(ZkSolcVersion::V1316),
         _ => Err(Error::msg(
             "ZkSolc compiler version not supported. Proper version format: 'v1.3.x'",
         )),
@@ -110,7 +110,7 @@ impl ZkSolcVersion {
             ZkSolcVersion::V1311 => "v1.3.11",
             ZkSolcVersion::V1313 => "v1.3.13",
             ZkSolcVersion::V1314 => "v1.3.14",
-            ZkSolcVersion::V1315 => "v1.3.15",
+            ZkSolcVersion::V1316 => "v1.3.16",
         }
     }
 }
@@ -217,7 +217,7 @@ impl ZkSolcManagerOpts {
 ///
 /// ```ignore
 /// use zkforge::zksolc_manager::{ZkSolcManagerBuilder, ZkSolcManagerOpts};
-/// let opts = ZkSolcManagerOpts::new("v1.3.15")
+/// let opts = ZkSolcManagerOpts::new("v1.3.16")
 /// let zk_solc_manager = ZkSolcManagerBuilder::new(opts)
 ///     .build()
 ///     .expect("Failed to build ZkSolcManager");
