9017b4bb4dded6dc1b04226e6c65821e61b85d2b
aon <21188659+aon@users.noreply.github.com>
2023-12-14 12:16:04 -0300

    feat: migrate era-revm cheatcodes

--- crates/era-cheatcodes/tests/src/cheatcodes/Serialize.t.sol
+++ crates/era-cheatcodes/tests/src/cheatcodes/Serialize.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Test, console2 as console} from "../../lib/forge-std/src/Test.sol";
 import {Constants} from "./Constants.sol";
+import {Utils} from "./Utils.sol";
 
 contract CheatcodeSerializeTest is Test {
     address constant TEST_ADDRESS = 0x6Eb28604685b1F182dAB800A1Bfa4BaFdBA8a79a;
@@ -17,7 +18,7 @@ contract CheatcodeSerializeTest is Test {
             )
         );
         require(success, "serializeAddress failed");
-        bytes memory data = trimReturnBytes(rawData);
+        bytes memory data = Utils.trimReturnBytes(rawData);
         string memory testString = string(abi.encodePacked(data));
         require(
             keccak256(bytes(testString)) ==
@@ -37,7 +38,7 @@ contract CheatcodeSerializeTest is Test {
             )
         );
         require(success, "serializeBool failed");
-        bytes memory data = trimReturnBytes(rawData);
+        bytes memory data = Utils.trimReturnBytes(rawData);
         string memory testString = string(abi.encodePacked(data));
         require(
             keccak256(bytes(testString)) == keccak256(bytes("true")),
@@ -56,28 +57,12 @@ contract CheatcodeSerializeTest is Test {
             )
         );
         require(success, "serializeUint failed");
-        bytes memory data = trimReturnBytes(rawData);
+        bytes memory data = Utils.trimReturnBytes(rawData);
         string memory testString = string(abi.encodePacked(data));
         require(
             keccak256(bytes(testString)) == keccak256(bytes("99")),
             "serializeUint mismatch"
         );
         console.log("failed?", failed());
-    }
-
-    function trimReturnBytes(
-        bytes memory rawData
-    ) internal pure returns (bytes memory) {
-        uint256 lengthStartingPos = rawData.length - 32;
-        bytes memory lengthSlice = new bytes(32);
-        for (uint256 i = 0; i < 32; i++) {
-            lengthSlice[i] = rawData[lengthStartingPos + i];
-        }
-        uint256 length = abi.decode(lengthSlice, (uint256));
-        bytes memory data = new bytes(length);
-        for (uint256 i = 0; i < length; i++) {
-            data[i] = rawData[i];
-        }
-        return data;
     }
 }
