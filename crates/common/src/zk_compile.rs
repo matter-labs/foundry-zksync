904f5453988136055d32c2a3e16686bdf22ff852
Nisheeth Barthwal <nbaztec@gmail.com>
2023-12-15 14:10:05 +0100

    feat: decouple env, use storage for era_test_node block and timestamp, enable fuzz testing (#195)

--- crates/common/src/zk_compile.rs
+++ crates/common/src/zk_compile.rs
@@ -203,7 +203,7 @@ pub fn compile_smart_contracts(
 
     match zksolc.compile() {
         Ok(_) => {
-            println!("Compiled Successfully");
+            info!("Compiled Successfully");
             Ok(())
         }
         Err(err) => {
@@ -718,7 +718,7 @@ impl ZkSolc {
         if has_error {
             exit(1);
         } else if has_warning {
-            println!("Compiler run completed with warnings");
+            warn!("Compiler run completed with warnings");
         }
     }
     /// Handles and formats the errors present in the output JSON from the zksolc compiler.
