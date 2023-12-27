1fd43bf36c6e085987c0e68afc6f8510aee5a5da
Herman Obst Demaestri <hodemaestri@gmail.com>
2023-12-27 20:40:57 -0300

    make test more expresive

--- crates/era-cheatcodes/tests/src/cheatcodes/Prank.t.sol
+++ crates/era-cheatcodes/tests/src/cheatcodes/Prank.t.sol
@@ -5,16 +5,29 @@ import {Test, console2 as console} from "../../lib/forge-std/src/Test.sol";
 import {Constants} from "./Constants.sol";
 
 contract PrankTest is Test {
-     address constant TEST_ADDRESS = 0x6Eb28604685b1F182dAB800A1Bfa4BaFdBA8a79a;
+    address constant TEST_ADDRESS = 0x6Eb28604685b1F182dAB800A1Bfa4BaFdBA8a79a;
+    address constant TEST_ORIGIN = 0xdEBe90b7BFD87Af696B1966082F6515a6E72F3d8;
     function testPrankSender() public {
-        
+        address original_msg_sender = msg.sender;
+        address original_tx_origin = tx.origin;       
         // Perform the prank
         PrankVictim victim = new PrankVictim();
-        Constants.CHEATCODE_ADDRESS.call(
+
+       // Verify that the victim is set up correctly
+        victim.assertCallerAndOrigin(
+            address(this),
+            "Prank failed: victim.assertCallerAndOrigin failed",
+            original_tx_origin,
+            "Prank failed: victim.assertCallerAndOrigin failed"
+        );
+
+        (bool success1, ) = Constants.CHEATCODE_ADDRESS.call(
             abi.encodeWithSignature("prank(address)", TEST_ADDRESS)
         );
+        require(success1, "Prank failed");
+
         victim.assertCallerAndOrigin( 
-            sender,
+            TEST_ADDRESS,
             "msg.sender was not set during prank",
             tx.origin,
             "tx.origin invariant failed"
@@ -61,7 +74,7 @@ contract PrankVictim {
     ) public view {
         console.log("msg.sender", msg.sender);
         console.log("tx.origin", tx.origin);
-        // require(msg.sender == expectedSender, senderMessage);
-        // require(tx.origin == expectedOrigin, originMessage);
+        require(msg.sender == expectedSender, senderMessage);
+        require(tx.origin == expectedOrigin, originMessage);
     }
 }
