4ca19cadec8b30866e4d779d62c816c2dcb650f1
Nisheeth Barthwal <nbaztec@gmail.com>
2024-03-07 21:07:34 +0100

    fix clippy

--- crates/forge/tests/cli/config.rs
+++ crates/forge/tests/cli/config.rs
@@ -124,6 +124,14 @@ forgetest!(can_extract_config_values, |prj, cmd| {
         isolate: true,
         __non_exhaustive: (),
         __warnings: vec![],
+        compiler_path: Default::default(),
+        zk_optimizer: Default::default(),
+        mode: Default::default(),
+        zk_optimizer_details: Default::default(),
+        fallback_oz: Default::default(),
+        is_system: Default::default(),
+        force_evmla: Default::default(),
+        detect_missing_libraries: Default::default(),
     };
     prj.write_config(input.clone());
     let config = cmd.config();
