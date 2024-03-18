2821a01103d6411a38765e46fa359f5986e505a1
Anton Levochko <hykakdela@gmail.com>
2024-03-18 15:41:29 +0200

    Fix cargo fmt

--- crates/common/src/zk_compile.rs
+++ crates/common/src/zk_compile.rs
@@ -1053,8 +1053,8 @@ impl ZkSolc {
     /// The versioned sources can then be used for further processing or analysis.
     fn get_versioned_sources(&mut self) -> Result<BTreeMap<Solc, SolidityVersionSources>> {
         // Step 1: Retrieve Project Sources
-        let sources = self.project.paths.read_input_files()
-            .wrap_err("Could not read input files")?;
+        let sources =
+            self.project.paths.read_input_files().wrap_err("Could not read input files")?;
 
         // Step 2: Resolve Graph of Sources and Versions
         let graph = Graph::resolve_sources(&self.project.paths, sources)
