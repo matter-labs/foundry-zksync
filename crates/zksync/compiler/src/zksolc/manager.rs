06570f5eabd65db4a7ad2c9fd05b45ffe3f78ce0
elfedy <federico@moonsonglabs.com>
2024-05-14 18:00:20 -0300

    Support multiple compiler versions

--- crates/zksync/compiler/src/zksolc/manager.rs
+++ crates/zksync/compiler/src/zksolc/manager.rs
@@ -1,33 +1,4 @@
 #![allow(missing_docs)]
-//! The `ZkSolcManager` module manages the downloading and setup of the `zksolc` Solidity
-/// compiler. This module provides functionalities to interact with different versions of the
-/// zksolc compiler as well as supporting different operating systems.
-///
-/// This module consists of several key components:
-///
-/// * `ZkSolcVersion`: This enum represents the available versions of the zksolc compiler. The
-///   `parse_version` function accepts a string version and returns the corresponding
-///   `ZkSolcVersion` variant.
-///
-/// * `ZkSolcOS`: This enum represents the supported operating systems for the zksolc compiler.
-///   `get_operating_system` function determines the current operating system and returns the
-///   corresponding `ZkSolcOS` variant.
-///
-/// * `ZkSolcManagerOpts`: This structure holds options for creating a `ZkSolcManagerBuilder`,
-///   which includes the version of the compiler to be used.
-///
-/// * `ZkSolcManagerBuilder`: This structure is used to construct a `ZkSolcManager`. It holds
-///   options like compilers path, version, compiler name, and download URL. It has a `build`
-///   function which constructs a `ZkSolcManager` instance.
-///
-/// * `ZkSolcManager`: This structure manages a particular version of the zksolc compiler. It
-///   includes functions to get the full compiler name, check if the compiler exists, setup the
-///   compilers directory, and download the compiler if necessary.
-///
-/// This module abstracts the details of managing the zksolc compiler, making it easier for
-/// developers to use different versions of the compiler without dealing with the details of
-/// downloading, setting up, and switching between versions. It is part of a larger framework
-/// for managing and interacting with zkSync contracts.
 use anyhow::{anyhow, Context, Error, Result};
 use reqwest::Client;
 use serde::Serialize;
@@ -38,17 +9,6 @@ use url::Url;
 const ZKSOLC_DOWNLOAD_BASE_URL: &str =
     "https://github.com/matter-labs/zksolc-bin/releases/download/";
 
-/// `ZkSolcVersion` is an enumeration of the supported versions of the `zksolc` compiler.
-///
-/// Each variant in this enum represents a specific version of the `zksolc` compiler:
-/// * `V138` corresponds to version 1.3.8 of the `zksolc` compiler.
-/// * `V139` corresponds to version 1.3.9 of the `zksolc` compiler.
-///
-/// The `get_version` method is provided to get a string representation of the version associated
-/// with each enum variant.
-///
-/// This enumeration is used in the `ZkSolcManager` to specify the `zksolc` compiler version to be
-/// used for contract compilation.
 #[derive(Debug, Clone, Serialize)]
 pub enum ZkSolcVersion {
     V135,
@@ -73,17 +33,6 @@ pub enum ZkSolcVersion {
 
 pub const DEFAULT_ZKSOLC_VERSION: &str = "v1.4.1";
 
-/// `parse_version` parses a string representation of a `zksolc` compiler version
-/// and returns the `ZkSolcVersion` enum variant if it matches a supported version.
-///
-/// # Arguments
-///
-/// * `version`: A string slice of the `zksolc` version to parse.
-///
-/// # Returns
-///
-/// A `Result` with the `ZkSolcVersion` variant for the parsed version, or an `Err`
-/// if the version isn't supported.
 fn parse_version(version: &str) -> Result<ZkSolcVersion> {
     match version {
         "v1.3.5" => Ok(ZkSolcVersion::V135),
@@ -111,11 +60,6 @@ fn parse_version(version: &str) -> Result<ZkSolcVersion> {
 }
 
 impl ZkSolcVersion {
-    /// `get_version` returns a string slice representing the `ZkSolcVersion` variant.
-    ///
-    /// # Returns
-    ///
-    /// A string slice representing the `ZkSolcVersion` variant.
     fn get_version(&self) -> &str {
         match self {
             ZkSolcVersion::V135 => "v1.3.5",
@@ -147,16 +91,6 @@ enum ZkSolcOS {
     MacARM,
 }
 
-/// `get_operating_system` identifies the current operating system and returns the corresponding
-/// `ZkSolcOS` variant.
-///
-/// # Returns
-///
-/// `ZkSolcOS` variant representing the current operating system.
-///
-/// # Errors
-///
-/// If the operating system is not supported, it returns an error.
 fn get_operating_system() -> Result<ZkSolcOS> {
     match std::env::consts::OS {
         "linux" => Ok(ZkSolcOS::Linux),
@@ -169,16 +103,6 @@ fn get_operating_system() -> Result<ZkSolcOS> {
 }
 
 impl ZkSolcOS {
-    /// `get_compiler` provides the appropriate compiler string based on the current operating
-    /// system.
-    ///
-    /// # Returns
-    ///
-    /// A string representing the compiler, depending on the operating system.
-    ///
-    /// # Note
-    ///
-    /// This function is used to construct the filename for the zkSync compiler binary.
     fn get_compiler(&self) -> &str {
         match self {
             ZkSolcOS::Linux => "zksolc-linux-amd64-musl-",
@@ -187,16 +111,6 @@ impl ZkSolcOS {
         }
     }
 
-    /// `get_download_uri` provides the appropriate URI for downloading the compiler binary based on
-    /// the current operating system.
-    ///
-    /// # Returns
-    ///
-    /// A string representing the URI, depending on the operating system.
-    ///
-    /// # Note
-    ///
-    /// This function is used to construct the URI for downloading the zkSync compiler binary.
     fn get_download_uri(&self) -> &str {
         match self {
             ZkSolcOS::Linux => "linux-amd64-musl",
@@ -217,36 +131,6 @@ impl ZkSolcManagerOpts {
     }
 }
 
-/// `ZkSolcManagerBuilder` is a structure that helps in creating a `ZkSolcManager` instance.
-///
-/// This builder pattern is used to encapsulate all the details needed to create a `ZkSolcManager`.
-/// The fields include:
-///
-/// * `compilers_path`: An optional `PathBuf` that denotes the directory where the compiler binaries
-///   are stored.
-/// * `version`: A string that represents the version of the zkSync compiler to be used.
-/// * `compiler`: An optional string that describes the compiler name.
-/// * `download_url`: The base URL from where the zkSync compiler binary is to be downloaded.
-///
-/// The builder provides a `new` function to initialize the structure with `ZkSolcManagerOpts`, and
-/// a `build` function which constructs and returns a `ZkSolcManager` instance.
-///
-/// The `get_compiler` function is used to determine the appropriate compiler name based on the
-/// operating system.
-///
-/// The builder follows a fluent interface design, allowing the setting of properties in a chained
-/// manner. The `build` function should be the final method invoked after all necessary properties
-/// have been set.
-///
-/// # Example
-///
-/// ```ignore
-/// use zkforge::zksolc_manager::{ZkSolcManagerBuilder, ZkSolcManagerOpts};
-/// let opts = ZkSolcManagerOpts::new("v1.3.23")
-/// let zk_solc_manager = ZkSolcManagerBuilder::new(opts)
-///     .build()
-///     .expect("Failed to build ZkSolcManager");
-/// ```
 #[derive(Debug, Clone)]
 pub struct ZkSolcManagerBuilder {
     _compilers_path: Option<PathBuf>,
@@ -256,26 +140,6 @@ pub struct ZkSolcManagerBuilder {
 }
 
 impl ZkSolcManagerBuilder {
-    /// Constructs a new instance of `ZkSolcManagerBuilder` using the provided `ZkSolcManagerOpts`.
-    ///
-    /// This function takes a `ZkSolcManagerOpts` instance as a parameter, which contains the zkSync
-    /// compiler version as a string. The builder's `version` field is initialized with this
-    /// version. The `compilers_path` and `compiler` fields are initialized as `None` since they
-    /// are optional and can be set later.
-    ///
-    /// The `download_url` field is initialized with the constant `ZKSOLC_DOWNLOAD_BASE_URL`, which
-    /// is the base URL from where the compiler binary will be downloaded.
-    ///
-    /// This function should be used as the starting point for creating a `ZkSolcManager` instance
-    /// using the builder pattern.
-    ///
-    /// # Parameters
-    ///
-    /// * `opts`: A `ZkSolcManagerOpts` instance containing the zkSync compiler version.
-    ///
-    /// # Returns
-    ///
-    /// Returns a new `ZkSolcManagerBuilder` instance.
     pub fn new(opts: ZkSolcManagerOpts) -> Self {
         Self {
             _compilers_path: None,
@@ -285,55 +149,12 @@ impl ZkSolcManagerBuilder {
         }
     }
 
-    /// Returns the appropriate compiler string based on the current operating system.
-    ///
-    /// This function determines the current operating system using `get_operating_system`, and
-    /// returns the corresponding compiler string based on the operating system. The compiler
-    /// string is used to construct the filename for the `zksolc` compiler binary.
-    ///
-    /// # Returns
-    ///
-    /// Returns a `Result<String>` representing the compiler string based on the current operating
-    /// system.
-    ///
-    /// # Errors
-    ///
-    /// This function can return an `Err` if the operating system cannot be determined using
-    /// `get_operating_system`.
     fn get_compiler(self) -> Result<String> {
         get_operating_system()
             .with_context(|| "Failed to determine OS for compiler")
             .map(|it| it.get_compiler().to_string())
     }
 
-    /// `build` constructs and returns a `ZkSolcManager` instance based on the provided
-    /// configuration options.
-    ///
-    /// This function finalizes the construction of a `ZkSolcManager` instance using the builder
-    /// pattern. It validates the provided options, resolves the necessary details, and creates
-    /// the manager instance.
-    ///
-    /// The function performs the following steps:
-    /// 1. Obtains the home directory path and appends the `.zksync` directory to it, which
-    ///    represents the compilers directory.
-    /// 2. Parses the provided version string and verifies if it matches one of the supported
-    ///    `ZkSolcVersion` variants.
-    /// 3. Determines the appropriate compiler string based on the current operating system using
-    ///    the `get_compiler` function.
-    /// 4. Constructs a `ZkSolcManager` instance with the resolved compilers directory, version,
-    ///    compiler, and download URL.
-    ///
-    /// # Returns
-    ///
-    /// A `Result` containing the constructed `ZkSolcManager` instance if the build process is
-    /// successful, or an `Err` if any errors occur.
-    ///
-    /// # Errors
-    ///
-    /// The function can return an `Err` in the following cases:
-    /// * If the home directory path cannot be determined.
-    /// * If the provided version string cannot be parsed into a valid `ZkSolcVersion` variant.
-    /// * If the current operating system is not supported or cannot be determined.
     pub fn build(self) -> Result<ZkSolcManager> {
         // TODO: try catching & returning errors quickly (rather than doing 'long' if and return
         // else at the end)
@@ -351,57 +172,6 @@ impl ZkSolcManagerBuilder {
     }
 }
 
-/// `ZkSolcManager` is a structure that manages a specific version of the `zksolc` Solidity
-/// compiler.
-///
-/// This structure encapsulates the functionality to interact with different versions of the
-/// `zksolc` compiler, including downloading, setting up, and checking for the existence of the
-/// compiler. It provides an abstraction for managing the `zksolc` compiler, making it easier for
-/// developers to use different versions without dealing with the low-level details.
-///
-/// # Fields
-///
-/// * `compilers_path`: A `PathBuf` representing the directory where the compiler binaries are
-///   stored.
-/// * `version`: A `ZkSolcVersion` representing the specific version of the `zksolc` compiler
-///   managed by this instance.
-/// * `compiler`: A `String` representing the compiler name.
-/// * `download_url`: A `Url` representing the base URL from which the `zksolc` compiler binary is
-///   downloaded.
-///
-/// # Example
-///
-/// ```ignore
-/// let compilers_path = PathBuf::from("/path/to/compilers");
-/// let version = ZkSolcVersion::V1323;
-/// let compiler = "zksolc-linux-amd64-musl-v1.33".to_string();
-/// let download_url = Url::parse("https://github.com/matter-labs/zksolc-bin/releases/download/").unwrap();
-///
-/// let zksolc_manager = ZkSolcManager::new(compilers_path, version, compiler, download_url);
-/// ```
-///
-/// The example above demonstrates the creation of a `ZkSolcManager` instance. It specifies the
-/// `compilers_path` where the compiler binaries are stored, the `version` of the `zksolc` compiler,
-/// the `compiler` name, and the `download_url` for the compiler binary.
-///
-/// # Implementation Details
-///
-/// The `ZkSolcManager` structure provides several methods to interact with the `zksolc` compiler:
-///
-/// * `get_full_compiler()`: Returns a `String` representing the full compiler name, including the
-///   version.
-/// * `get_full_download_url()`: Returns a `Result<Url>` representing the full download URL for the
-///   compiler binary.
-/// * `get_full_compiler_path()`: Returns a `PathBuf` representing the full path to the compiler
-///   binary.
-/// * `exists()`: Checks if the compiler binary exists in the compilers directory.
-/// * `check_setup_compilers_dir()`: Checks and sets up the compilers directory if it doesn't exist.
-/// * `download()`: Downloads the compiler binary if it doesn't already exist in the compilers
-///   directory.
-///
-/// The `ZkSolcManager` structure provides a high-level interface to manage the `zksolc` compiler,
-/// simplifying the process of handling different versions and ensuring the availability of the
-/// compiler for contract compilation.
 #[derive(Debug, Clone)]
 pub struct ZkSolcManager {
     compilers_path: PathBuf,
@@ -434,18 +204,6 @@ impl fmt::Display for ZkSolcManager {
     }
 }
 
-/// The `setup_zksolc_manager` function creates and prepares an instance of `ZkSolcManager`.
-///
-/// It follows these steps:
-/// 1. Instantiate `ZkSolcManagerOpts` and `ZkSolcManagerBuilder` with the specified zkSync Solidity
-///    compiler.
-/// 2. Create a `ZkSolcManager` using the builder.
-/// 3. Check if the setup compilers directory is properly set up. If not, it raises an error.
-/// 4. If the zkSync Solidity compiler does not exist in the compilers directory, it triggers its
-///    download.
-///
-/// The function returns the `ZkSolcManager` if all steps are successful, or an error if any
-/// step fails.
 pub async fn setup_zksolc_manager(zksolc_version: String) -> eyre::Result<PathBuf> {
     let zksolc_manager_opts = ZkSolcManagerOpts::new(zksolc_version.clone());
     let zksolc_manager_builder = ZkSolcManagerBuilder::new(zksolc_manager_opts);
