8c1abd16f945715f2852b5ec4f8cdd1a4a419a8a
Francesco Dainese <franci.dainese@gmail.com>
2024-06-03 19:22:01 +0200

    refactor(vm): `inspect` in separate module

--- crates/zksync/core/src/vm/mod.rs
+++ crates/zksync/core/src/vm/mod.rs
@@ -1,11 +1,13 @@
 mod db;
 mod env;
 mod farcall;
+mod inspect;
 mod runner;
 mod storage_view;
 mod tracer;
 mod tx;
 
+pub use inspect::{inspect, ZKVMExecutionResult, ZKVMResult};
 pub use runner::{balance, call, code_hash, create, encode_create_params, nonce, transact};
 pub use tracer::CheatcodeTracerContext;
 pub use tx::split_tx_by_factory_deps;
