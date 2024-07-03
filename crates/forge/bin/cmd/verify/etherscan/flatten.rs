21c0062eeb5ede2bb6e297a57817af60530103dc
Karrq <francesco@moonsonglabs.com>
2024-07-03 16:03:08 +0200

    feat(verify): zksync contract verification (#440)

--- crates/forge/bin/cmd/verify/etherscan/flatten.rs
+++ crates/forge/bin/cmd/verify/etherscan/flatten.rs
@@ -3,9 +3,14 @@ use eyre::{Context, Result};
 use foundry_block_explorers::verify::CodeFormat;
 use foundry_compilers::{
     artifacts::{BytecodeHash, Source},
+    zksync::{
+        artifacts::{BytecodeHash as ZkBytecodeHash, CompilerInput as ZkCompilerInput},
+        compile::{output::AggregatedCompilerOutput as ZkAggregatedCompilerOutput, ZkSolc},
+    },
     AggregatedCompilerOutput, CompilerInput, Project, Solc,
 };
 use semver::{BuildMetadata, Version};
+
 use std::{collections::BTreeMap, path::Path};
 
 #[derive(Debug)]
@@ -32,6 +37,39 @@ impl EtherscanSourceProvider for EtherscanFlattenedSource {
         if !args.force {
             // solc dry run of flattened code
             self.check_flattened(source.clone(), version, target).map_err(|err| {
+                eyre::eyre!(
+                    "Failed to compile the flattened code locally: `{}`\
+            To skip this solc dry, have a look at the `--force` flag of this command.",
+                    err
+                )
+            })?;
+        }
+
+        let name = args.contract.name.clone();
+        Ok((source, name, CodeFormat::SingleFile))
+    }
+
+    fn zk_source(
+        &self,
+        args: &VerifyArgs,
+        project: &Project,
+        target: &Path,
+        version: &Version,
+    ) -> Result<(String, String, CodeFormat)> {
+        let metadata = project.zksync_zksolc_config.settings.metadata.as_ref();
+        let bch = metadata.and_then(|m| m.bytecode_hash).unwrap_or_default();
+
+        eyre::ensure!(
+            bch == ZkBytecodeHash::Keccak256,
+            "When using flattened source with zksync, bytecodeHash must be set to keccak256 because Etherscan uses Keccak256 in its Compiler Settings when re-compiling your code. BytecodeHash is currently: {}. Hint: Set the bytecodeHash key in your foundry.toml :)",
+            bch,
+        );
+
+        let source = project.flatten(target).wrap_err("Failed to flatten contract")?;
+
+        if !args.force {
+            // solc dry run of flattened code
+            self.zk_check_flattened(source.clone(), version, target).map_err(|err| {
                 eyre::eyre!(
                     "Failed to compile the flattened code locally: `{}`\
             To skip this solc dry, have a look at the `--force` flag of this command.",
@@ -87,6 +125,55 @@ impl EtherscanFlattenedSource {
 Failed to compile the flattened code locally.
 This could be a bug, please inspect the output of `forge flatten {}` and report an issue.
 To skip this solc dry, pass `--force`.
+Diagnostics: {diags}",
+                contract_path.display()
+            );
+        }
+
+        Ok(())
+    }
+
+    /// Attempts to compile the flattened content locally with the zksolc compiler version.
+    ///
+    /// This expects the completely flattened `content´ and will try to compile it using the
+    /// provided compiler. If the compiler is missing it will be installed.
+    ///
+    /// # Errors
+    ///
+    /// If it failed to install a missing solc compiler
+    ///
+    /// # Exits
+    ///
+    /// If the solc compiler output contains errors, this could either be due to a bug in the
+    /// flattening code or could to conflict in the flattened code, for example if there are
+    /// multiple interfaces with the same name.
+    fn zk_check_flattened(
+        &self,
+        content: impl Into<String>,
+        version: &Version,
+        contract_path: &Path,
+    ) -> Result<()> {
+        let version = strip_build_meta(version.clone());
+        let zksolc = ZkSolc::find_installed_version(&version)?
+            .unwrap_or(ZkSolc::blocking_install(&version)?);
+
+        let input = ZkCompilerInput {
+            language: "Solidity".to_string(),
+            sources: BTreeMap::from([("contract.sol".into(), Source::new(content))]),
+            settings: Default::default(),
+        };
+
+        let (out, _) = zksolc.compile(&input)?;
+        if out.has_error() {
+            let mut o = ZkAggregatedCompilerOutput::default();
+            o.extend(version, out);
+            let diags = o.diagnostics(&[], &[], Default::default());
+
+            eyre::bail!(
+                "\
+Failed to compile the flattened code locally.
+This could be a bug, please inspect the output of `forge flatten {}` and report an issue.
+To skip this zksolc dry, pass `--force`.
 Diagnostics: {diags}",
                 contract_path.display()
             );
