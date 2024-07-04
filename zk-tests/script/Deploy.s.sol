319340a9f8663c7a9e43ec6bb1e55bc6caff085e
Francesco Dainese <franci.dainese@gmail.com>
2024-07-05 00:41:19 +0200

    test(zk): static-call test

--- zk-tests/script/Deploy.s.sol
+++ zk-tests/script/Deploy.s.sol
@@ -2,41 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
-import {Test} from "forge-std/Test.sol";
-import {console2 as console} from "forge-std/console2.sol";
-
-contract Greeter {
-    string name;
-    uint256 age;
-
-    event Greet(string greet);
-
-    function greeting(string memory _name) public returns (string memory) {
-        name = _name;
-        string memory greet = string(abi.encodePacked("Hello ", _name));
-        emit Greet(greet);
-        return greet;
-    }
-
-    function greeting2(
-        string memory _name,
-        uint256 n
-    ) public returns (uint256) {
-        name = _name;
-        string memory greet = string(abi.encodePacked("Hello ", _name));
-        console.log(name);
-        emit Greet(greet);
-        return n * 2;
-    }
-
-    function setAge(uint256 _age) public {
-        age = _age;
-    }
-
-    function getAge() public view returns (uint256) {
-        return age;
-    }
-}
+import {Greeter} from "../src/Greeter.sol";
 
 contract DeployScript is Script {
     // Vm constant vm = Vm(HEVM_ADDRESS);
@@ -51,9 +17,16 @@ contract DeployScript is Script {
         );
         require(success, "zkVm() call failed");
         vm.startBroadcast();
+
         greeter = new Greeter();
-        greeter.greeting("john");
+
         greeter.setAge(123);
+        uint256 age = greeter.getAge();
+
+        greeter.greeting("john");
+
         vm.stopBroadcast();
+
+        assert(age == 123);
     }
 }
--- zk-tests/src/Contracts.t.sol
+++ zk-tests/src/Contracts.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Test, console2 as console} from "forge-std/Test.sol";
 import {ConstantNumber} from "./ConstantNumber.sol";
+import {Greeter} from "./Greeter.sol";
 
 interface ISystemContractDeployer {
     function getNewAddressCreate2(
@@ -299,5 +300,33 @@ contract ZkContractsTest is Test {
 
         assertEq(42, multiNumber.one());
         assertEq(2, multiNumber.two());
+    }
+
+    function testZkStaticCalls() public {
+        (bool success, ) = address(vm).call(
+            abi.encodeWithSignature("zkVm(bool)", true)
+        );
+        require(success, "zkVm() call failed");
+        address sender = address(this);
+        uint64 startingNonce = vm.getNonce(sender);
+
+        //this ensures calls & deployments increase the nonce
+        vm.startBroadcast(sender);
+
+        Greeter greeter = new Greeter();
+        assert(vm.getNonce(sender) == startingNonce + 1);
+
+        greeter.setAge(42);
+        assert(vm.getNonce(sender) == startingNonce + 2);
+
+        // static-call, nonce shouldn't change
+        uint256 age = greeter.getAge();
+        assert(age == 42);
+        assert(vm.getNonce(sender) == startingNonce + 2);
+
+        uint256 num = greeter.greeting2("zksync", 2);
+        assert(num == 4);
+        assert(vm.getNonce(sender) == startingNonce + 3);
+        vm.stopBroadcast();
     }
 }
