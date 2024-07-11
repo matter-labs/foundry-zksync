74b51dbb1eada6080464e47a0a6a8ea43c11ead9
Herman Obst Demaestri <hodemaestri@gmail.com>
2024-07-11 16:57:21 -0300

    use correct gas price for fee estimation. use gas_per_pubdata_limit in requestEip712tx. WIP debugging signature and encoding

--- crates/forge/bin/cmd/script/broadcast.rs
+++ crates/forge/bin/cmd/script/broadcast.rs
@@ -109,15 +109,18 @@ impl ScriptArgs {
             let sequential_broadcast =
                 send_kind.signers_count() != 1 || self.slow || !has_batch_support(chain);
 
-            debug!("HERMAN: Passing scripting");
+            debug!("HERMAN: THIS GAS ESTIMATION IN WRONG");
             // Make a one-time gas price estimation
             let (gas_price, eip1559_fees) = {
                 match deployment_sequence.transactions.front().unwrap().typed_tx() {
                     TypedTransaction::Eip1559(_) => {
+                        debug!("HERMAN: ENTERING EIP1559 TX");
                         let fees = estimate_eip1559_fees(&provider, Some(chain))
                             .await
                             .wrap_err("Failed to estimate EIP1559 fees. This chain might not support EIP1559, try adding --legacy to your command.")?;
 
+                        
+                        debug!("HERMAN FEEES {:?}", fees);
                         (None, Some(fees))
                     }
                     _ => (provider.get_gas_price().await.ok(), None),
@@ -136,9 +139,11 @@ impl ScriptArgs {
 
                     let kind = send_kind.for_sender(&from)?;
                     let is_fixed_gas_limit = tx_with_metadata.is_fixed_gas_limit;
+                    debug!("HERMAN: is fixed gas limit? {is_fixed_gas_limit}");
                     let zk = tx_with_metadata.zk.clone();
 
                     let mut tx = tx.clone();
+                    debug!("HERMAN: TYPEDTX: {:?}",tx);
 
                     tx.set_chain_id(chain);
 
@@ -146,10 +151,12 @@ impl ScriptArgs {
                         tx.set_gas_price(gas_price.to_ethers());
                     } else {
                         // fill gas price
+                        debug!("HERMAN: FILLING GAS PRICE");
                         match tx {
                             TypedTransaction::Eip1559(ref mut inner) => {
                                 let eip1559_fees =
                                     eip1559_fees.expect("Could not get eip1559 fee estimation.");
+                                debug!("HERMAN: EIP1559 2: {:?}", eip1559_fees);
                                 if let Some(priority_gas_price) = self.priority_gas_price {
                                     inner.max_priority_fee_per_gas =
                                         Some(priority_gas_price.to_ethers());
@@ -163,6 +170,7 @@ impl ScriptArgs {
                             }
                         }
                     }
+                    debug!("HERMAN: EIP1559 TX: {:?}", tx);
 
                     Ok((tx, zk, kind, is_fixed_gas_limit))
                 })
@@ -646,6 +654,7 @@ impl ScriptArgs {
             // if already set, some RPC endpoints might simply return the gas value that is
             // already set in the request and omit the estimate altogether, so
             // we remove it here
+            debug!("HERMAN: ENTERING HERE?");
             let _ = legacy_or_1559.gas_mut().take();
 
             self.estimate_gas(&mut legacy_or_1559, &provider).await?;
@@ -654,33 +663,39 @@ impl ScriptArgs {
         let signed_tx = if let Some(zk) = zk {
             let custom_data = Eip712Meta::new().factory_deps(zk.factory_deps);
 
+
+            let gas_price = provider.get_gas_price().await?;
             let mut deploy_request = Eip712TransactionRequest::new()
                 .r#type(EIP712_TX_TYPE)
                 .from(*legacy_or_1559.from().unwrap())
                 .to(*legacy_or_1559.to().and_then(|to| to.as_address()).unwrap())
                 .chain_id(legacy_or_1559.chain_id().unwrap().as_u64())
                 .nonce(legacy_or_1559.nonce().unwrap())
-                .gas_price(legacy_or_1559.gas_price().unwrap())
+                .gas_price(gas_price)
                 .max_fee_per_gas(legacy_or_1559.max_cost().unwrap())
                 .data(legacy_or_1559.data().cloned().unwrap())
                 .custom_data(custom_data);
 
-            let gas_price = provider.get_gas_price().await?;
+            debug!("HERMAN: PROVIDER GAS PRICE: {gas_price}");
             let fee: zksync_web3_rs::zks_provider::types::Fee =
                 provider.request("zks_estimateFee", [deploy_request.clone()]).await.unwrap();
+
+            debug!("HERMAN: FEE = {:?}", fee);
             deploy_request = deploy_request
                 .gas_limit(fee.gas_limit)
                 .max_fee_per_gas(fee.max_fee_per_gas)
-                .max_priority_fee_per_gas(fee.max_priority_fee_per_gas)
-                .gas_price(gas_price);
+                .max_priority_fee_per_gas(fee.max_priority_fee_per_gas);
+            deploy_request.custom_data.gas_per_pubdata = fee.gas_per_pubdata_limit;
 
             let signable: Eip712Transaction =
                 deploy_request.clone().try_into().expect("converting deploy request");
+
             debug!("sending transaction: {:?}", signable);
 
             let signature =
                 signer.sign_typed_data(&signable).await.wrap_err("Failed to sign typed data")?;
 
+            debug!("HERMAN: ENCODING:");
             let encoded_rlp =
                 &*deploy_request.rlp_signed(signature).expect("able to rlp encode deploy request");
             [&[EIP712_TX_TYPE], encoded_rlp].concat().into()
@@ -696,6 +711,7 @@ impl ScriptArgs {
 
         // Submit the raw transaction
         let pending = provider.send_raw_transaction(signed_tx).await?;
+        debug!("HERMAN: NOT ENTERING {:?}", pending);
 
         Ok(pending.tx_hash().to_alloy())
     }
