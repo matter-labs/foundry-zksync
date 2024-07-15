c9d27b9616e43f595f6ce56b6471b81cc5cf8e65
Herman Obst Demaestri <70286869+HermanObst@users.noreply.github.com>
2024-07-15 15:09:21 -0300

    Update crates/forge/bin/cmd/script/broadcast.rs
    Co-authored-by: Nisheeth Barthwal <nbaztec@gmail.com>

--- crates/forge/bin/cmd/script/broadcast.rs
+++ crates/forge/bin/cmd/script/broadcast.rs
@@ -675,7 +675,7 @@ impl ScriptArgs {
 
             // TODO: This is a work around as try_into is not propagating
             // gas_per_pubdata_byte_limit. It always set the default We would need to
-            // fix taht library or add EIP712 to alloy with correct implementation.
+            // fix that library or add EIP712 to alloy with correct implementation.
             let mut signable: Eip712Transaction =
                 deploy_request.clone().try_into().expect("converting deploy request");
             signable.gas_per_pubdata_byte_limit = deploy_request.custom_data.gas_per_pubdata;
