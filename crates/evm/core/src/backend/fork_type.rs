eed4c01ed49af53293a445147dad60dc85f8582e
Nisheeth Barthwal <nbaztec@gmail.com>
2024-07-30 16:58:15 +0200

    create reactor

--- crates/evm/core/src/backend/fork_type.rs
+++ crates/evm/core/src/backend/fork_type.rs
@@ -38,7 +38,11 @@ impl CachedForkType {
         let is_zk_url = foundry_common::provider::try_get_http_provider(fork_url)
             .map(|provider| {
                 let is_zk_url = std::thread::spawn(move || {
-                    futures::executor::block_on(provider.raw_request("zks_L1ChainId".into(), ()))
+                    tokio::runtime::Builder::new_multi_thread()
+                        .enable_all()
+                        .build()
+                        .unwrap()
+                        .block_on(provider.raw_request("zks_L1ChainId".into(), ()))
                         .map(|_: String| true)
                         .unwrap_or_default()
                 })
