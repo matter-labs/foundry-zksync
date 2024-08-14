fe044aa180999f6bd46b8944e69f32425d625a39
Francesco Dainese <franci.dainese@gmail.com>
2024-08-14 19:35:05 +0200

    test(zk): recordLogs cheatcode

--- crates/forge/tests/it/zk/cheats.rs
+++ crates/forge/tests/it/zk/cheats.rs
@@ -90,3 +90,11 @@ async fn test_zk_cheat_works_after_fork() {
 
     TestConfig::with_filter(runner, filter).evm_spec(SpecId::SHANGHAI).run().await;
 }
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_zk_record_logs() {
+    let runner = TEST_DATA_DEFAULT.runner_zksync();
+    let filter = Filter::new("RecordLogs", "ZkCheatcodesTest", ".*");
+
+    TestConfig::with_filter(runner, filter).evm_spec(SpecId::SHANGHAI).run().await;
+}
