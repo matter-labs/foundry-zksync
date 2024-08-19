221c2dadd290254de338dee6a580a6422029a226
Karrq <francesco@moonsonglabs.com>
2024-08-19 22:31:22 +0200

    test(zk): AAVE-DI (#519)
    * test(zk): add aave-di
    
    * test(ci): remove aave-di
    
    * chore: rename
    
    ---------
    
    Co-authored-by: Juan Rigada <62958725+Jrigada@users.noreply.github.com>

--- crates/forge/tests/cli/ext_integration.rs
+++ crates/forge/tests/cli/ext_integration.rs
@@ -144,3 +144,10 @@ fn convex_shutdown_simulation() {
     .fork_block(14445961)
     .run();
 }
+
+#[test]
+fn test_zk_aave_di() {
+    ExtTester::new("Moonsong-Labs", "aave-delivery-infrastructure", "ci")
+        .args(["--zksync", "--avoid-contracts", "\"*/PayloadScripts.t.sol\""])
+        .run()
+}
