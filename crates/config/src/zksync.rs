7c0cd97e8d7a27a7ca8447f5bee1d9b1dc02c01e
Juan Rigada <62958725+Jrigada@users.noreply.github.com>
2024-08-23 12:13:34 -0300

    Update crates/config/src/zksync.rs
    Co-authored-by: Federico Rodríguez <federico@moonsonglabs.com>

--- crates/config/src/zksync.rs
+++ crates/config/src/zksync.rs
@@ -129,6 +129,7 @@ impl ZkSyncConfig {
             },
         };
 
+         // `cli_settings` get set from `Project` values when building `ZkSolcVersionedInput` 
         ZkSolcSettings { settings: zk_settings, cli_settings: CliSettings::default() }
     }
 
