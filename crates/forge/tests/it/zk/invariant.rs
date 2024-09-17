0ac687a1e018a929f38a9865e34fa5b158779e9f
Francesco Dainese <franci.dainese@gmail.com>
2024-09-17 20:47:43 +0200

    test(zk): proper basic invariant test

--- crates/forge/tests/it/zk/invariant.rs
+++ crates/forge/tests/it/zk/invariant.rs
@@ -1,13 +1,19 @@
 //! Invariant tests
 
-use crate::{config::*, test_helpers::TEST_DATA_DEFAULT};
+use crate::{config::*, test_helpers::{ForgeTestData, ForgeTestProfile}};
 use forge::revm::primitives::SpecId;
 use foundry_test_utils::Filter;
 
 #[tokio::test(flavor = "multi_thread")]
 async fn test_zk_invariant_deposit() {
-    let runner = TEST_DATA_DEFAULT.runner_zksync();
-    let filter = Filter::new("testZkInvariantDeposit", "ZkInvariantTest", ".*");
+    let mut test_data = ForgeTestData::new(ForgeTestProfile::Default);
+    // FIXME: just use the inline config
+    test_data.test_opts.invariant.no_zksync_reserved_addresses = true;
+    test_data.test_opts.invariant.fail_on_revert = true;
+    test_data.test_opts.invariant.runs = 10;
+
+    let runner = test_data.runner_zksync();
+    let filter = Filter::new(".*", "ZkInvariantTest", ".*");
 
     TestConfig::with_filter(runner, filter).evm_spec(SpecId::SHANGHAI).run().await;
 }
