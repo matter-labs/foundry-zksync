babed9c09dcd3b21d18249d041c5c92a5a8e47fd
Karrq <Karrq@users.noreply.github.com>
2024-09-19 16:52:44 +0000

    deploy: da77402c298066b084547c6027f1c9e3104fe871

---
+++ trait.impl/alloy_consensus/transaction/trait.Transaction.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["anvil_core",[["impl Transaction for <a class=\"struct\" href=\"anvil_core/eth/transaction/optimism/struct.DepositTransactionRequest.html\" title=\"struct anvil_core::eth::transaction::optimism::DepositTransactionRequest\">DepositTransactionRequest</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[257]}
