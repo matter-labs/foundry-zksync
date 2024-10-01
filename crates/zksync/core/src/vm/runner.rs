905aa28dd9416681570fadc0dafd3ce7bbd30fe3
Jrigada <jrigada@frba.utn.edu.ar>
2024-10-01 14:41:33 -0300

    extract variable is_paymaster_used

--- crates/zksync/core/src/vm/runner.rs
+++ crates/zksync/core/src/vm/runner.rs
@@ -264,8 +264,10 @@ where
     }
     let max_fee_per_gas = fix_l2_gas_price(ecx.env.tx.gas_price.to_u256());
 
+    let is_paymaster_used = !paymaster_params.paymaster.is_zero();
+
     // We check if the paymaster is set, if it is not set, we use the proposed gas limit
-    let gas_limit = if paymaster_params.paymaster.is_zero() {
+    let gas_limit = if is_paymaster_used {
         ecx.env.tx.gas_limit.into()
     } else {
         fix_l2_gas_limit(ecx.env.tx.gas_limit.into(), max_fee_per_gas, value, balance)
