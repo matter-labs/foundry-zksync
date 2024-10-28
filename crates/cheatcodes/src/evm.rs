0d444fab24a0aa9e9597504a9da8bb1b1c9cef0c
elfedy <federico@moonsonglabs.com>
2024-10-28 13:57:50 -0300

    chore: remove duplicated RecordAccess

--- crates/cheatcodes/src/evm.rs
+++ crates/cheatcodes/src/evm.rs
@@ -30,30 +30,6 @@ pub(crate) mod mapping;
 pub(crate) mod mock;
 pub(crate) mod prank;
 
-/// Records storage slots reads and writes.
-#[derive(Clone, Debug, Default)]
-pub struct RecordAccess {
-    /// Storage slots reads.
-    pub reads: HashMap<Address, Vec<U256>>,
-    /// Storage slots writes.
-    pub writes: HashMap<Address, Vec<U256>>,
-}
-
-impl RecordAccess {
-    /// Records a read access to a storage slot.
-    pub fn record_read(&mut self, target: Address, slot: U256) {
-        self.reads.entry(target).or_default().push(slot);
-    }
-
-    /// Records a write access to a storage slot.
-    ///
-    /// This also records a read internally as `SSTORE` does an implicit `SLOAD`.
-    pub fn record_write(&mut self, target: Address, slot: U256) {
-        self.record_read(target, slot);
-        self.writes.entry(target).or_default().push(slot);
-    }
-}
-
 /// Records the `snapshotGas*` cheatcodes.
 #[derive(Clone, Debug)]
 pub struct GasRecord {
