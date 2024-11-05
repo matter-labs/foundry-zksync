55fa29436e6ca25c0f5df7cb86a097133cb45d14
Nisheeth Barthwal <nbaztec@gmail.com>
2024-11-05 20:56:54 +0100

    fix: update zksync deps for upstream merge 57bb12e (#701)
    * update zksync deps

--- crates/forge/tests/cli/build.rs
+++ crates/forge/tests/cli/build.rs
@@ -125,7 +125,9 @@ forgetest_init!(build_sizes_no_forge_std, |prj, cmd| {
 forgetest_init!(test_zk_build_sizes, |prj, cmd| {
     cmd.args(["build", "--sizes", "--zksync", "--evm-version", "shanghai"]);
     let stdout = cmd.assert_success().get_output().stdout_lossy();
-    let pattern = Regex::new(r"\|\s*Counter\s*\|\s*800\s*\|\s*800\s*\|\s*450,199\s*\|\s*450,199\s*\|").unwrap();
+    let pattern =
+        Regex::new(r"\|\s*Counter\s*\|\s*800\s*\|\s*800\s*\|\s*450,199\s*\|\s*450,199\s*\|")
+            .unwrap();
 
     assert!(pattern.is_match(&stdout), "Unexpected size output:\n{stdout}");
 });
