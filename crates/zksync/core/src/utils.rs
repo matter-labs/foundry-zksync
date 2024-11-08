c9eb20c55bc5e1d0729a3630ff75929ed1fc2ec2
Juan Rigada <62958725+Jrigada@users.noreply.github.com>
2024-11-08 11:05:39 -0300

    chore: sync with upstream 58bf161 (#713)
    * chore(deps): prefer soft pinning on minor version for dependencies (#9269)
    
    * soft-pin on minor version, prefer workspace
    
    * use workspace
    
    * prefer workspace
    
    * Revert "fix(remappings): check if remapping to add starts with existing remapping name (#9246)" (#9274)
    
    This reverts commit 455ba9b1b736766232d84ba1790ac9ba6ca944de.
    
    * Fix build on upstream merge
    
    * chore: resolve merge conflicts for upstream-58bf161 (#708)
    
    * chore(deps): prefer soft pinning on minor version for dependencies (#9269)
    
    * soft-pin on minor version, prefer workspace
    
    * use workspace
    
    * prefer workspace
    
    * Revert "fix(remappings): check if remapping to add starts with existing remapping name (#9246)" (#9274)
    
    This reverts commit 455ba9b1b736766232d84ba1790ac9ba6ca944de.
    
    ---------
    
    Co-authored-by: zerosnacks <95942363+zerosnacks@users.noreply.github.com>
    Co-authored-by: grandizzy <38490174+grandizzy@users.noreply.github.com>
    
    * chore: fix build for upstream merge 58bf161 (#709)
    
    * chore(deps): prefer soft pinning on minor version for dependencies (#9269)
    
    * soft-pin on minor version, prefer workspace
    
    * use workspace
    
    * prefer workspace
    
    * Revert "fix(remappings): check if remapping to add starts with existing remapping name (#9246)" (#9274)
    
    This reverts commit 455ba9b1b736766232d84ba1790ac9ba6ca944de.
    
    * Fix build on upstream merge
    
    ---------
    
    Co-authored-by: zerosnacks <95942363+zerosnacks@users.noreply.github.com>
    Co-authored-by: grandizzy <38490174+grandizzy@users.noreply.github.com>
    
    * Apply format
    
    * Update rust-toolchain
    
    * Clippy
    
    * chore: Update rust version on CI (#714)
    
    update rust version on CI
    
    * Add back verification logic
    
    ---------
    
    Co-authored-by: zerosnacks <95942363+zerosnacks@users.noreply.github.com>
    Co-authored-by: grandizzy <38490174+grandizzy@users.noreply.github.com>

--- crates/zksync/core/src/utils.rs
+++ crates/zksync/core/src/utils.rs
@@ -112,6 +112,7 @@ pub fn fix_l2_gas_price(gas_price: U256) -> U256 {
 }
 
 /// Limits the gas_limit proportional to a user's available balance given the gas_price.
+///
 /// Additionally, fixes the gas limit to be maximum of 2^31, which is below the VM gas limit of
 /// 2^32. This is required so the bootloader does not throw an error for not having enough balance
 /// to pay for gas.
