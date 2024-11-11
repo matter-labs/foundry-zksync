0e80e10e96dd1f9099a79556ea0c1135163c6a2c
Francesco Dainese <franci.dainese@gmail.com>
2024-11-11 16:11:12 +0100

    test(zk): add tests with unlinked contracts

---
+++ testdata/zk/WithLibraries.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.8.7 <0.9.0;
+
+import "forge-std/console.sol";
+
+library Foo {
+   function foo() external pure {
+       console.log("I'm a library");
+   }
+}
+
+contract UsesFoo {
+    constructor() {
+        Foo.foo();
+    }
+}
