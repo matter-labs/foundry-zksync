4f0bdf0b1287bcaca6ab8025842164e5f3a263eb
Juan Rigada <62958725+Jrigada@users.noreply.github.com>
2024-11-29 10:24:21 -0300

    clarify clear message

--- crates/cheatcodes/src/inspector.rs
+++ crates/cheatcodes/src/inspector.rs
@@ -1282,7 +1282,7 @@ impl Cheatcodes {
             .collect_vec();
         factory_deps.extend(injected_factory_deps);
 
-        // NOTE(zk): Clear injected factory deps
+        // NOTE(zk): Clear injected factory deps so that they are not sent on further transactions
         self.zk_use_factory_deps.clear();
         tracing::debug!(contract = contract.name, "using dual compiled contract");
 
