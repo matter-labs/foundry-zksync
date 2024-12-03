0c9f2fcbcba35d45b89dbb6fe767b89e81f717eb
Jrigada <jrigada@frba.utn.edu.ar>
2024-12-03 14:19:11 -0300

    Cargo fmt

--- crates/cast/bin/main.rs
+++ crates/cast/bin/main.rs
@@ -45,8 +45,9 @@ fn main() {
 }
 
 fn run() -> Result<()> {
-    // NOTE(zk): We need to manually install the crypto provider as zksync-era uses `aws-lc-rs` provider, while
-    // foundry uses the `ring` provider. As a result, rustls cannot disambiguate between the two while selecting a default provider.
+    // NOTE(zk): We need to manually install the crypto provider as zksync-era uses `aws-lc-rs`
+    // provider, while foundry uses the `ring` provider. As a result, rustls cannot disambiguate
+    // between the two while selecting a default provider.
     let _ = rustls::crypto::ring::default_provider().install_default();
 
     handler::install();
