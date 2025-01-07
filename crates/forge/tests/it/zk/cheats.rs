9be4421ba2de27aa1cf98867545c8c9ebbe78635
Nisheeth Barthwal <nbaztec@gmail.com>
2025-01-07 13:11:34 +0100

    fix: do not handle expectCall in zkEVM (#807)
    * skip base call
    
    * add test
    
    ---------
    
    Co-authored-by: Federico Rodríguez <federico@moonsonglabs.com>

--- crates/forge/tests/it/zk/cheats.rs
+++ crates/forge/tests/it/zk/cheats.rs
@@ -80,6 +80,14 @@ async fn test_zk_cheat_expect_emit_works() {
 }
 
 #[tokio::test(flavor = "multi_thread")]
+async fn test_zk_cheat_expect_call_works() {
+    let runner = TEST_DATA_DEFAULT.runner_zksync();
+    let filter = Filter::new("testExpectCall", "ZkCheatcodesTest", ".*");
+
+    TestConfig::with_filter(runner, filter).spec_id(SpecId::SHANGHAI).run().await;
+}
+
+#[tokio::test(flavor = "multi_thread")]
 async fn test_zk_cheat_mock_with_value_function() {
     let runner = TEST_DATA_DEFAULT.runner_zksync();
     let filter = Filter::new("testZkCheatcodesValueFunctionMockReturn", "ZkCheatcodesTest", ".*");
