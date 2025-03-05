f8347c2da31b73898cd5081e6f2cb65f33a4ce7d
Karrq <francesco@moonsonglabs.com>
2025-03-05 18:23:27 +0100

    feat: upstream 4974a08 build (#936)
    * fix(inspector): duplicated import
    
    fix(inspector): missing field in default
    
    * feat(inspect:zk): `PathOrContractInfo` arg
    
    * deps: remove proptest patch
    
    deps: restore upstream `alloy-chains`
    
    * chore: fmt
    
    * fix(test:zk): `--no-commit` is now default
    
    * feat: track upstream foundry version in metadata
    
    * fix(test:zk): remove unnecessary `--no-commit`
    
    * fix(test): vyper released 0.4.1
    
    * chore: typo
    
    * ci: pin vyper (#9989)
    
    * Revert "fix(test): vyper released 0.4.1"
    
    This reverts commit 1b8884d4958660cf2e5579aaa96a1d5c8a51d511.
    
    * fix(test:zk:inspect): exclude logging output
    
    ---------
    
    Co-authored-by: DaniPopes <57450786+DaniPopes@users.noreply.github.com>

--- crates/forge/bin/cmd/inspect.rs
+++ crates/forge/bin/cmd/inspect.rs
@@ -81,7 +81,7 @@ impl InspectArgs {
             let should_compile_with_zksolc = zksync::check_command_for_field(&field)?;
             if should_compile_with_zksolc {
                 let config = Config { ..Default::default() };
-                return zksync::inspect(&field, config, target_path, &contract.name);
+                return zksync::inspect(&field, config, target_path, contract.name());
             }
         }
 
@@ -506,23 +506,23 @@ impl PartialEq<ContractOutputSelection> for ContractArtifactField {
         type Eos = EvmOutputSelection;
         matches!(
             (self, other),
-            (Self::Abi | Self::Events, Cos::Abi)
-                | (Self::Errors, Cos::Abi)
-                | (Self::Bytecode, Cos::Evm(Eos::ByteCode(_)))
-                | (Self::DeployedBytecode, Cos::Evm(Eos::DeployedByteCode(_)))
-                | (Self::Assembly | Self::AssemblyOptimized, Cos::Evm(Eos::Assembly))
-                | (Self::LegacyAssembly, Cos::Evm(Eos::LegacyAssembly))
-                | (Self::MethodIdentifiers, Cos::Evm(Eos::MethodIdentifiers))
-                | (Self::GasEstimates, Cos::Evm(Eos::GasEstimates))
-                | (Self::StorageLayout, Cos::StorageLayout)
-                | (Self::DevDoc, Cos::DevDoc)
-                | (Self::Ir, Cos::Ir)
-                | (Self::IrOptimized, Cos::IrOptimized)
-                | (Self::Metadata, Cos::Metadata)
-                | (Self::UserDoc, Cos::UserDoc)
-                | (Self::Ewasm, Cos::Ewasm(_))
-                | (Self::Eof, Cos::Evm(Eos::DeployedByteCode(_)))
-                | (Self::EofInit, Cos::Evm(Eos::ByteCode(_)))
+            (Self::Abi | Self::Events, Cos::Abi) |
+                (Self::Errors, Cos::Abi) |
+                (Self::Bytecode, Cos::Evm(Eos::ByteCode(_))) |
+                (Self::DeployedBytecode, Cos::Evm(Eos::DeployedByteCode(_))) |
+                (Self::Assembly | Self::AssemblyOptimized, Cos::Evm(Eos::Assembly)) |
+                (Self::LegacyAssembly, Cos::Evm(Eos::LegacyAssembly)) |
+                (Self::MethodIdentifiers, Cos::Evm(Eos::MethodIdentifiers)) |
+                (Self::GasEstimates, Cos::Evm(Eos::GasEstimates)) |
+                (Self::StorageLayout, Cos::StorageLayout) |
+                (Self::DevDoc, Cos::DevDoc) |
+                (Self::Ir, Cos::Ir) |
+                (Self::IrOptimized, Cos::IrOptimized) |
+                (Self::Metadata, Cos::Metadata) |
+                (Self::UserDoc, Cos::UserDoc) |
+                (Self::Ewasm, Cos::Ewasm(_)) |
+                (Self::Eof, Cos::Evm(Eos::DeployedByteCode(_))) |
+                (Self::EofInit, Cos::Evm(Eos::ByteCode(_)))
         )
     }
 }
