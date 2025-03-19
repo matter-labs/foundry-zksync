9650b5547df097604a93e8ba589013d8f40d7362
Juan Rigada <62958725+Jrigada@users.noreply.github.com>
2025-03-19 10:44:05 -0300

    Update crates/forge/tests/it/zk/cheats.rs
    Co-authored-by: Nisheeth Barthwal <nbaztec@gmail.com>

--- crates/forge/tests/it/zk/cheats.rs
+++ crates/forge/tests/it/zk/cheats.rs
@@ -88,9 +88,9 @@ async fn test_zk_cheat_expect_revert_works() {
 }
 
 #[tokio::test(flavor = "multi_thread")]
-async fn test_zk_cheat_expect_revert_works_with_external_call_reverts() {
+async fn test_zk_cheat_expect_revert_works_with_nested_call_reverts() {
     let runner = TEST_DATA_DEFAULT.runner_zksync();
-    let filter = Filter::new("testExpectRevertWithExternalCallReverts", "ZkCheatcodesTest", ".*");
+    let filter = Filter::new("testExpectRevertWithNestedCallReverts", "ZkCheatcodesTest", ".*");
 
     TestConfig::with_filter(runner, filter).spec_id(SpecId::SHANGHAI).run().await;
 }
