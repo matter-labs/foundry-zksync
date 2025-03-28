9cd9927853bb3807c0ceaa05beac46f3927c832c
Jrigada <jrigada@frba.utn.edu.ar>
2025-03-28 16:24:04 -0300

    Add api key for zk tests

--- crates/forge/tests/cli/config.rs
+++ crates/forge/tests/cli/config.rs
@@ -1043,7 +1043,7 @@ endpoints = "all"
 [profile.default.zksync]
 compile = false
 startup = false
-fallback_oz = false
+size_fallback = false
 enable_eravm_extensions = false
 force_evmla = false
 llvm_options = []
