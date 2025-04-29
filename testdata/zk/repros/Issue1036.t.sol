16c8373a918ad6100f11a535efaef0d22587ea58
Juan Rigada <62958725+Jrigada@users.noreply.github.com>
2025-04-29 09:48:36 -0300

    Update testdata/zk/repros/Issue1036.t.sol
    Co-authored-by: Nisheeth Barthwal <nbaztec@gmail.com>

--- testdata/zk/repros/Issue1036.t.sol
+++ testdata/zk/repros/Issue1036.t.sol
@@ -7,7 +7,7 @@ import "cheats/Vm.sol";
 contract Issue1036 is DSTest {
     Vm constant vm = Vm(HEVM_ADDRESS);
 
-    function test_failingForkViaWebsocket() public {
+    function test_forkSuceedsViaWebsocket() public {
         // The issue presented itself when using websocket endpoints and
         // not initializing properly the crypto provider.
         vm.createSelectFork("wss://mainnet.era.zksync.io/ws");
