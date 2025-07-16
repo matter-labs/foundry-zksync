00c419d7f841144f45d75bf0c2bad64a3782f49d
Matthias Seitz <matthias.seitz@outlook.de>
2025-07-16 17:47:09 +0200

    chore: relax tracer bounds (#11024)

--- crates/anvil/src/eth/backend/mem/inspector.rs
+++ crates/anvil/src/eth/backend/mem/inspector.rs
@@ -177,9 +177,9 @@ where
 
     #[inline]
     fn selfdestruct(&mut self, contract: Address, target: Address, value: U256) {
-        call_inspectors!([&mut self.tracer, &mut self.transfer], |inspector| {
-            Inspector::<CTX, EthInterpreter>::selfdestruct(inspector, contract, target, value)
-        });
+        if let Some(tracer) = &mut self.tracer {
+            <TracingInspector as Inspector<CTX>>::selfdestruct(tracer, contract, target, value);
+        }
     }
 }
 
