63d81adeceb7412682a2b6826f0564614bbbc6cc
Juan Rigada <62958725+Jrigada@users.noreply.github.com>
2025-09-01 13:56:47 -0300

    chore: upstream merge ffdc91bc5e (#1138)
    * Upstream merge ffdc91bc5e
    
    * Remove duplicated no-fail-fast flag
    
    * Ignore test
    
    * fix zk tests positional arguments due to new check
    
    * format
    
    * update spec

--- crates/cast/src/opts.rs
+++ crates/cast/src/opts.rs
@@ -1067,7 +1067,7 @@ pub enum CastSubcommand {
     #[command(visible_alias = "com")]
     Completions {
         #[arg(value_enum)]
-        shell: clap_complete::Shell,
+        shell: foundry_common::clap::Shell,
     },
 
     /// Generate Fig autocompletion spec.
